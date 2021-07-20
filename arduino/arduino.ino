0740f7648327e3dd6c2ad14b9201329a92a42b43
alejandro maciel <amaciel@fiuna.edu.py>
2021-07-19 21:42:45 -0400

    actualizacion de funciones

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,14 +1,54 @@
 #include <ctime>
+#include <AccelStepper.h>
+#include <SoftwareSerial.h>
+
+#define X_STEP_PIN         54
+#define X_DIR_PIN          55
+#define X_ENABLE_PIN       38
+#define X_MIN_PIN           3
+#define X_MAX_PIN           2
+
+#define Y_STEP_PIN         60
+#define Y_DIR_PIN          61
+#define Y_ENABLE_PIN       56
+#define Y_MIN_PIN          14
+#define Y_MAX_PIN          15
+
+#define Z_STEP_PIN         46
+#define Z_DIR_PIN          48
+#define Z_ENABLE_PIN       62
+#define Z_MIN_PIN          18
+#define Z_MAX_PIN          19
+
+#define servopin            1
+
+#define motor1pin1          5
+#define motor1pin2          6
+#define ENA                 4
+
+#define LED_rojo           50
+#define LED_verde          52
+#define pushbutton         A11
+
+//boolean x_done, y_done, z_done, done;
+
+AccelStepper stepperX = AccelStepper(1, X_STEP_PIN, X_DIR_PIN);
+AccelStepper stepperY = AccelStepper(1, Y_STEP_PIN, Y_DIR_PIN);
+AccelStepper stepperZ = AccelStepper(1, Z_STEP_PIN, Z_DIR_PIN);
+
+AccelStepper stepper[] = {AccelStepper (1, X_STEP_PIN, X_DIR_PIN), AccelStepper (1, Y_STEP_PIN, Y_DIR_PIN), AccelStepper (1, Z_STEP_PIN, Z_DIR_PIN) };
+
+int MIN_Switch[] = {X_MIN_PIN, Y_MIN_PIN, Z_MIN_PIN};
+int MAX_Switch[] = {X_MAX_PIN, Y_MAX_PIN, Z_MAX_PIN};
+
+Servo myservo;
 
 String x;
-int pushButton = 53;
 int estado = 0;
 int marcaEstado = 0;
+float dz = 4000;
 
-int LED_rojo = 52;
-int LED_verde = 51;
-
-int cinta = 50;
+int vel_cinta = 110;
 char uart_python[10];
 
 char posicion[10];
@@ -20,204 +60,253 @@ int fila, columna;
 // lugar 16 es la posicion inicial
 // lugar 17 es la posicion de entrega de medicamentos
 // lugar 18 es la posicion de la cinta
-lista_posiciones[19][3] = { { 0, 0, 0},// 0
-                            { 0, 0, 0},// 1
-                            { 0, 0, 0},// 2
-                            { 0, 0, 0},// 3
-                            { 0, 0, 0},// 4
-                            { 0, 0, 0},// 5
-                            { 0, 0, 0},// 6
-                            { 0, 0, 0},// 7
-                            { 0, 0, 0},// 8
-                            { 0, 0, 0},// 9
-                            { 0, 0, 0},// 10
-                            { 0, 0, 0},// 11
-                            { 0, 0, 0},// 12
-                            { 0, 0, 0},// 13
-                            { 0, 0, 0},// 14
-                            { 0, 0, 0},// 15
-                            { 0, 0, 0},// 16
-                            { 0, 0, 0},// 17 
-                            { 0, 0, 0} };// 18
+float lista_posiciones[19][3] = { { -2040, 1500, 38500},// 0
+                            { -1485, 1150, 38500},// 1
+                            { -895, 1150, 38500},// 2
+                            { -310, 1150, 38500},// 3
+                            { -2040, 1150, 27200},// 4
+                            { -1485, 1150, 27200},// 5
+                            { -895, 1150, 27200},// 6
+                            { -310, 1150, 27200},// 7
+                            { -2040, 1150, 15400},// 8
+                            { -1485, 1150, 15400},// 9
+                            { -895, 1150, 15400},// 10
+                            { -310, 1150, 15400},// 11
+                            { -2040, 1150, 3600},// 12
+                            { -1485, 1150, 3600},// 13
+                            { -895, 1150, 3600},// 14
+                            { -310, 1150, 3600},// 15
+                            { 0, 0, 0},// 16 //final de carrera minimo
+                            { 0, 0, 0},// 17 //final de carrera minimo
+                            { 0, 0, 0} };// 18 //final de carrera maximo
 int posicion_final[3];
 
-// variables del motor (NO DEFINITIVO)
-int motor_x=0,motor_y=0,motor_z=0;
+
+void entregarRemedio(){
+    done = 0;
+    stepper[0].moveTo(-4000);
+    while ( done == 0) {
+      if(digitalRead(MAX_Switch[0]) == LOW){
+        stepper[0].stop();
+        done = 1;
+      }
+      else if (stepper[0].distanceToGo() != 0){
+        stepper[0].setSpeed(-250);
+        stepper[0].run();
+      }
+      else 
+        done = 1;
+    }
+
+    // mover servo
+    int posServo = 0;
+    for (posServo = 0; posServo <= 180; posServo += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+      myservo.write(posServo);              // tell servo to go to position in variable 'pos'
+      delay(500);                       // waits 15ms for the servo to reach the position
+    }
+    for (posServo = 180; posServo >= 0; posServo -= 1) { // goes from 180 degrees to 0 degrees
+      myservo.write(posServo);              // tell servo to go to position in variable 'pos'
+      delay(500);                       // waits 15ms for the servo to reach the position
+  }
+}
+
+void homeStepper (){
+      //Y
+     done = 0;
+    stepper[1].moveTo(-2000);
+    while ( done == 0) {
+      if(digitalRead(MIN_Switch[1]) == LOW){
+        stepper[1].stop();
+        stepper[1].setCurrentPosition(0);
+        done = 1;
+      }
+      else if (stepper[1].distanceToGo() != 0){
+        stepper[1].setSpeed(-250);
+        stepper[1].run();
+      }
+      else 
+        done = 1;
+    }
+
+    //Z
+    done = 0;
+    stepper[2].moveTo(-60000);
+    while ( done == 0) {
+      if(digitalRead(MIN_Switch[2]) == LOW){
+        stepper[2].stop();
+        stepper[2].setCurrentPosition(0);
+        done = 1;
+      }
+      else if (stepper[2].distanceToGo() != 0){
+        stepper[2].setSpeed(-1000);
+        stepper[2].run();
+      }
+      else 
+        done = 1;
+    }
+
+    //X
+    done = 0;
+    stepper[0].moveTo(4000);
+    while ( done == 0) {
+      if(digitalRead(MIN_Switch[0]) == LOW){
+        stepper[0].stop();
+        stepper[0].setCurrentPosition(0);
+        done = 1;
+      }
+      else if (stepper[0].distanceToGo() != 0){
+        stepper[0].setSpeed(250);
+        stepper[0].run();
+      }
+      else 
+        done = 1;
+    }
+}
 
 
-void empujar_hasta_que_caiga(void){
-    int a = 0;
-    return;
+void moveStepper (int i, double cant, int vel){
+   done = 0 ;
+   
+  stepper[i].moveTo(cant);
+  while ( done == 0) {
+    if(digitalRead(MAX_Switch[i]) == LOW){
+      stepper[i].stop();
+      done = 1;
+    }
+    else if (stepper[i].distanceToGo() != 0){
+      stepper[i].setSpeed(vel);
+      stepper[i].run();
+    }
+    else 
+      done = 1;
+  }
+}
+
+void mover_cinta(){
+   digitalWrite(motor1pin1, LOW);
+   digitalWrite(motor1pin2, HIGH);
+
+   analogWrite(ENA, vel_cint); 
+   delay(5000);
+   
+    digitalWrite(motor1pin1, LOW);
+   digitalWrite(motor1pin2, LOW);
+  }
+
+
+int integerButton(button){
+  int bandera;
+  val = analogRead(button);
+  percent = map(val, 0, 1023, 0, 100);
+
+  if (percent > 90){
+    bandera = 1;
+    return bandera;
+  }
+  else{
+    bandera = 0;
+    return bandera;
+  }
 }
 
 
 void setup() {
+
+  stepper[0].setEnablePin(X_ENABLE_PIN);
+  stepper[0].setPinsInverted(false, false, true);
+  stepper[0].enableOutputs();
+
+  stepper[1].setEnablePin(Y_ENABLE_PIN);
+  stepper[1].setPinsInverted(false, false, true);
+  stepper[1].enableOutputs();
+
+  stepper[2].setEnablePin(Z_ENABLE_PIN);
+  stepper[2].setPinsInverted(false, false, true);
+  stepper[2].enableOutputs();
+  
+  stepper[0].setMaxSpeed(350);
+  stepper[1].setMaxSpeed(350);
+  stepper[2].setMaxSpeed(2000);
+ 
+  pinMode(X_MIN_PIN, INPUT);
+  pinMode(Y_MIN_PIN, INPUT);
+  pinMode(Z_MIN_PIN, INPUT);
+
+  pinMode(X_MAX_PIN, INPUT);
+  pinMode(Y_MAX_PIN, INPUT);
+  pinMode(Z_MAX_PIN, INPUT);
+  
     Serial.begin(115200);
-    pinMode(pushButton, INPUT);
+    
+    //pinMode(pushButton, INPUT);
     pinMode(LED_rojo, OUTPUT);
     pinMode(LED_verde, OUTPUT);
-    pinMode(cinta, OUTPUT);
+    //pinMode(cinta, OUTPUT);
+
+    pinMode(ENA,OUTPUT);
+    pinMode(motor1pin1, OUTPUT);
+    pinMode(motor1pin2, OUTPUT);
 }
 
 
 //-----------------------MAIN-------------------------
 void loop() {
-    // Pedido de Medicamento
+    // Despacho de Medicamento///////////////////////////////////////////
     // 1. Primero debe verificar si hay pedidos
     posicion = Serial.read();
     digitalWrite(LED_rojo, LOW);
     digitalWrite(LED_verde, HIGH);
+
+    homeStepper();
     
     if (posicion){
         digitalWrite(LED_rojo, HIGH);
         digitalWrite(LED_verde, LOW);
         
-        fila = (int) posicion[1];
-        columna = (int) posicion[3];
-        posicion_final = lista_posiciones[4*fila+columna][:];
+        posicion_final = lista_posiciones[posicion][:];
         
         // 2. Se mueven los motores de a 1 para no forzarle demasiado al sistema
-        //motor_y ponele tiene que llegar primero
-        while (posi_y < posicion_final[1]){
-                motor_y = 255;
-        }
-        motor_y = 0;
-        
-        //motor_x se mueve segundo
-        while (posi_x < posicion_final[0]){
-                motor_x = 255;
-        }
-        motor_x = 0;
-        
-        //motor_z se mueve tercero
-        while (posi_z < posicion_final[2]){
-                motor_z = 255;
-        }
-        motor_z = 0;
-        
-        
-        //3. Se hace el proceso de levantar el medicamento
+  
+         moveStepper(0, posicion_final[0], -350); // 0 is stepperX
+         delay(500);
+         moveStepper(2, posicion_final[2], 1000); // 2 is stepperZ
+         delay(500);
+         moveStepper(1, posicion_final[1], 350); // 1 is stepperY
+         delay(500); 
+
+         //3. Se hace el proceso de levantar el medicamento
         // Primero se mete el robot bajo el palé
-        //Entra
-        while (posi_x < posicion_final[0]+dx){
-                motor_x = 255;
-        }
-        motor_x = 0;
-        
-        //Levanta
-        while (posi_z < posicion_final[2]+dz){
-            motor_z = 255;
-        }
-        motor_z = 0;
-        
-        //Retrocede
-        while (posi_x > posicion_final[0]){
-            motor_x = -255;
-        }
-        motor_x = 0;
+         moveStepper(2, posicion_final[2]+dz, 1000); // 2 is stepperZ
         
         // 4. Lleva el medicamento en la posicion de entrega de medicamentos
-        posicion_final = lista_posiciones[17];
-        
-        //motor_z se mueve primero
-        while (posi_z > posicion_final[2]){
-                motor_z = -255;
-                
-        }
-        motor_z = 0;
-        
-        //motor_x se mueve segundo
-        while (posi_x > posicion_final[0]){
-                motor_x = -255;
-        }
-        motor_x = 0;
-        
-        //motor_y se mueve tercero
-        while (posi_y > posicion_final[1]){
-                motor_y = -255;
-        }
-        motor_y = 0;
-        
-        // Se empuja el medicamento para que caiga
-        empujar_hasta_que_caiga();
-        
+        entregarRemedio();
+         
         // 5. Se vuelve a la posición inicial
-        posicion_final = lista_posiciones[16];
-        
-        //motor_z se mueve primero
-        while (posi_z > posicion_final[2]){
-                motor_z = -255;
-                
-        }
-        motor_z = 0;
-        
-        //motor_x se mueve segundo
-        while (posi_x > posicion_final[0]){
-                motor_x = -255;
-        }
-        motor_x = 0;
-        
-        //motor_y se mueve tercero
-        while (posi_y > posicion_final[1]){
-                motor_y = -255;
-        }
-        motor_y = 0;
+        homeStepper();
     }
 
-    // Reposición de medicamentos
+    // Reposición de medicamentos////////////////////////////////////////////////
     // 1. Primero se verifica si se presionó el botón
-    if (digitalRead(pushButton) == HIGH){
+    boton = int integerButton(pushButton)
+    if (boton == HIGH){
         digitalWrite(LED_rojo, HIGH);
         digitalWrite(LED_verde, LOW);
+        cinta();
         
-        // 2. Se empieza a mover la cinta y se verifica si llegó al punto de escaneo
-        // El mensaje de escaneo es "escaneo"
-        int bandera_scan = 0;
-        time_t init_time;
-        time_t current_time;
-        while(true){
-            uart_python = Serial.read();
-            digitalWrite(cinta, HIGH);
-            delay(15);
-            if (uart_python == "escaneo"){
-                bandera_scan = 1;
-                init_time = time(NULL);
-                posicion = Serial.read();
-            }
-            // Se hace más lenta la cinta al principio y dp de escanear a toda puta
-            if (bandera_scan == 0){
-                digitalWrite(cinta,LOW);
-                delay(15);
-            }
-            
-            // Se espera 5 segundos para que llegue el remedio en su lugar
-            current_time = time(NULL) - init_time;
-            if (current_time < 100 && current_time > 4){
-                break;
-            }
+        // 2. El robot lleva el medicamento donde debe ser
+        posicion_final = lista_posiciones[posicion][:];
+  
+         moveStepper(0, posicion_final[0], -350); // 0 is stepperX
+         delay(500);
+         moveStepper(2, posicion_final[2]+dz, 1000); // 2 is stepperZ
+         delay(500);
+         moveStepper(1, posicion_final[1], 350); // 1 is stepperY
+         delay(500); 
+         moveStepper(2, posicion_final[2], -1000); // 2 is stepperZ
 
-        }
-        
-        // 3. El robot lleva el medicamento donde debe ser
-        fila = (int) posicion[1];
-        columna = (int) posicion[3];
-        posicion_final = lista_posiciones[4*fila+columna][:];
-        while (posi_y < posicion_final[1]){
-                motor_y = 255;
-        }
-        motor_y = 0;
-        
-        //motor_x se mueve segundo
-        while (posi_x < posicion_final[0]){
-                motor_x = 255;
-        }
-        motor_x = 0;
-        
-        //motor_z se mueve tercero
-        while (posi_z < posicion_final[2]){
-                motor_z = 255;
-        }
-        motor_z = 0;
+       // 3. Se vuelve a la posición inicial
+        homeStepper();
         
     }
 }
